bc8b5bee03de59ed3af96b4a0087c48df7415a1f
Steve Anderson <prototype909@msn.com>
2020-07-16 00:13:24 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ApplicationRecord
+  include ActiveModel::Validations
   validates :title, presence: true
-  validates :title, uniqueness: { scope: [:artist_name, :release_year]}
+  #validates :title, uniqueness: { scope: [:artist_name, :release_year]}
 
   validates :artist_name, presence: true
 
